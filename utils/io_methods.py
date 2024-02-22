6be8cec94d0b4fa885610c6573a97961ab883142
Hadi Kutabi <hadi.kutabi@kit.edu>
2024-02-22 09:36:58 +0100

    added cls to dumpy and load json methods

--- utils/io_methods.py
+++ utils/io_methods.py
@@ -2,14 +2,14 @@ import pickle
 import json
 
 
-def load_json(path, mode="r"):
+def load_json(path, mode="r", decoder_cls=None):
     with open(path, mode) as f:
-        return json.load(f)
+        return json.load(f, cls=decoder_cls)
 
 
-def dump_json(obj, path, mode="w"):
+def dump_json(obj, path, mode="w", encoder_cls=None):
     with open(path, mode) as f:
-        json.dump(obj, f, indent=4)
+        json.dump(obj, f, indent=4, cls=encoder_cls)
 
 
 def load_pickle(path, mode="rb"):
